1eed1b68e928fb670fbdcc705ddbae8d4f53c1ee
luisking96 <luistecisc@gmail.com>
2023-03-27 23:00:56 -0600

    Add nav-movil palete-colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: theme =>({
+				...theme('colors'),
+				'primary': '#0A2647',
+        'secund': '#144272',
+        'third': '#205295',
+        'base': '#2C74B3',
+        'backg': '#e2e8f0'
+			}),
+      textColor:{
+				'text-1': '#e2e8f0',
+        'text-2': '#0f172a',
+			},
+    },
   },
   plugins: [],
 }
